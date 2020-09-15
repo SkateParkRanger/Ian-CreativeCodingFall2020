5402197b94185fea5acd8ae408b90ada3f8e3bf9
Ian <ideatrick@hotmail.com>
2020-09-15 12:36:58 -0400

    Interactive mouse

--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -1,23 +1,12 @@
-let xpos = 0;
-let ypos = 0;
-
-let xpos2 = 100;
-let ypos2=50;
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(windowWidth,windowHeight);
 
-  angleMode(DEGREES);
-}
+  }
 
 function draw() {
-  background (100,0,200);
-  rect (xpos,ypos,50);
-  xpos = xpos +1;
-  ypos = ypos +5;
-
-  circle (xpos2,ypos2,50);
-  xpos2 = xpos2 + 3;
-  ypos2 = ypos2 + 1;
+background(100,0,200);
+  fill(mouseX,mouseY,0);
+  circle(mouseX,mouseY,70);
 
 }
